5a54ff49ee5e64d742ac8dfc63dbbc63ba804e23
OlegSkalGit <olegskalgit@github.com>
2026-06-15 14:37:44 +0300

    Stop tracking example directory

--- example/1 in/e12.bat
+++
@@ -1,2 +0,0 @@
-"..\..\.venv\Scripts\python.exe" "..\..\_templates_machine_.py" "e1.xlsx" "e2.xlsx"
-pause
--- example/1 in/w12.bat
+++
@@ -1,2 +0,0 @@
-"..\..\.venv\Scripts\python.exe" "..\..\_templates_machine_.py" "w1.docx" "w2.docx"
-pause
